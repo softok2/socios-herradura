db2b6b71b09e66b294dab67fa24ad1a2e5671eb4
Softok2 <shouddy@gmail.com>
2026-06-25 10:51:25 -0600

    Adding project's files

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+importScripts("https://www.gstatic.com/firebasejs/12.3.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.3.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDtR6p0WlUJMY8N8I-92qApZ4mIJCRbxEE",
+  authDomain: "demosgolf.firebaseapp.com",
+  projectId: "demosgolf",
+  storageBucket: "demosgolf.firebasestorage.app",
+  messagingSenderId: "493930171532",
+  appId: "1:493930171532:web:3bb1807d44c23119c2bfa9",
+  measurementId: "G-DZJCRRGGM3",
+});
+
+firebase.messaging();
